edd953f837ae58950da0ff2ab56cb2bf5f5e56d6
OAK-Graphics <78473748+OAK-Graphics@users.noreply.github.com>
2021-03-12 10:12:31 -0600

    Update on css and js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ function cardHover(t){
 
     const cardItemAll = document.querySelectorAll('.card__item');
     const cardImageAll = document.querySelectorAll('#cardImage');
+    const cardNthChild = document.querySelectorAll('.card__item:nth-child(n)');
     const cardFirstChild = document.querySelector('.card__item:nth-child(1)');
     const cardSecondChild = document.querySelector('.card__item:nth-child(2)');
     const cardThirdChild = document.querySelector('.card__item:nth-child(3)');
+    const imageNthChild = document.querySelectorAll('#cardImage:nth-child(n)');
     const imageFirstChild = document.querySelector('#cardImage:nth-child(1)');
     const imageSecondChild = document.querySelector('#cardImage:nth-child(2)');
     const imageThirdChild = document.querySelector('#cardImage:nth-child(3)');
+    const imageArray = Array.prototype.slice.call(imageNthChild);
 
     cardItemAll.forEach(e => {
 
@@ -20,55 +23,60 @@ function cardHover(t){
 
     t.classList.add('active');
 
-    if (t == cardFirstChild) {
+    imageArray.forEach(function(i) {
 
         cardImageAll.forEach(e => {
 
             if (e.classList.contains('active')) {
     
-                e.classList.remove('active');            
-            }
-            
-        });
-
-        imageFirstChild.classList.add('active');
-
-        console.log(t);
-
-    }
-
-    if (t == cardSecondChild) {
-
-        cardImageAll.forEach(e => {
-
-            if (e.classList.contains('active')) {
+                e.classList.remove('active');
     
-                e.classList.remove('active');            
-            }
+                e.classList.remove('show');  
+
+                e.classList.add('hide'); 
+
+            } 
             
         });
 
-        imageSecondChild.classList.add('active');
-
-        console.log(t);
-
-    }
-
-    if (t == cardThirdChild) {
-
-        cardImageAll.forEach(e => {
-
-            if (e.classList.contains('active')) {
+        if (t == cardFirstChild) {
     
-                e.classList.remove('active');            
-            }
+            imageFirstChild.classList.remove('hide');
             
-        });
+            imageFirstChild.classList.add('show');
+    
+            imageFirstChild.classList.add('active');
+    
+            console.log(t);
+    
+        }
+    
+        if (t == cardSecondChild) {
+    
+            imageSecondChild.classList.remove('hide');
+            
+            imageSecondChild.classList.add('show');
+    
+            imageSecondChild.classList.add('active');
+    
+            console.log(t);
+    
+        }
+    
+        if (t == cardThirdChild) {
+    
+            imageThirdChild.classList.remove('hide');
+            
+            imageThirdChild.classList.add('show');
+    
+            imageThirdChild.classList.add('active');
+    
+            console.log(t);
+    
+        }
 
-        imageThirdChild.classList.add('active');
-
-        console.log(t);
-
-    }
+        console.log(i);
+        
+    });
 
 }
